88dfc226a19bd8e9eac78daaf40f451cfef13b4d
PGP 2F93D5AF <wikijm@users.noreply.github.com>
2016-11-18 16:47:00 +0100

    Add Disconnect-Office365_PSSession

--- utilities/Office365.ps1
+++ utilities/Office365.ps1
@@ -1,10 +1,57 @@
-function Connect-EXOnline {
-    $URL = "https://ps.outlook.com/powershell"
-    $Credentials = Get-Credential -Message "Enter your Office 365 admin credentials"
-    $EXOSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri $URL -Credential $Credentials -Authentication Basic -AllowRedirection -Name "Exchange Online"
-    Import-PSSession $EXOSession
+<#
+	.SYNOPSIS
+		Connect to Office 365 Cloud Services into active PowerShell shell
+	
+	.PARAMETER Office365_URL
+		URL to Office 365 Cloud Services for PowerShell
+	
+	.PARAMETER Office365_Credentials
+		Capture administrative credential for future connections
+	
+#>
+function Connect-Office365_PSSession
+{
+	[CmdletBinding()]
+	param
+	(
+		[Parameter(Mandatory = $true)]
+		[string]$Office365_URL = "https://ps.outlook.com/powershell",
+		[Parameter(Mandatory = $true)]
+		$Office365_Credentials
+	)
+	
+	#Imports the installed Azure Active Directory module.
+	Import-Module MSOnline
+	
+	#Capture administrative credential for future connections.
+	$Office365_Credentials = Get-Credential -Message "Enter your Office 365 admin credentials"
+	
+	#Establishes Online Services connection to Office 365 Management Layer.
+	Connect-MsolService -Credential $Office365_Credentials
+	
+	#Creates an Exchange Online session using defined credential.
+	$EXOSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri $Office365_URL -Credential $Office365_Credentials -Authentication Basic -AllowRedirection -Name "Exchange Online"
+	
+	#This imports the Office 365 session into your active Shell.
+	Import-PSSession $EXOSession
 }
 
-function Disconnect-EXOnline {
-    Remove-PSSession -Name "Exchange Online"
+
+<#
+	.SYNOPSIS
+		Disconnect PowerShell session to Office 365 Cloud Services
+	
+	.PARAMETER PSSession_Name
+		Default name is "Exchange Online"
+
+#>
+function Disconnect-Office365_PSSession
+{
+	param
+	(
+		[Parameter(Mandatory = $true)]
+		[string]$PSSession_Name
+	)
+	
+	Remove-PSSession -Name $PSSession_Name
 }
