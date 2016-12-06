2ec4c5073ae14dbd252bba714d86d0de10061290
PGP 2F93D5AF <wikijm@users.noreply.github.com>
2016-12-06 11:33:43 +0100

    Add "Get-MyPublicIP" function

--- utilities/SystemStatus.ps1
+++ utilities/SystemStatus.ps1
@@ -76,6 +76,11 @@ Function Ping-Host
     }
 }
 
+Function Get-MyPublicIP {
+(Invoke-WebRequest -Uri ifconfig.me).RawContent -match "b(?:d{1,3}.){3}d{1,3}b" | Out-Null
+$matches | select -ExpandProperty Values
+}
+
 # This function gets your System Uptime
 Function Get-Uptime
 {
