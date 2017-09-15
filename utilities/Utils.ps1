b7b2767f07ac97abbec40c68e407f4d597a1069a
PGP 2F93D5AF <wikijm@users.noreply.github.com>
2017-09-15 07:57:14 +0200

    Add Show-MessageBox function + minor correction

--- utilities/Utils.ps1
+++ utilities/Utils.ps1
@@ -1,9 +1,164 @@
-﻿function OnApplicationLoad {
+function OnApplicationLoad {
 	return $true #return true for success or false for failure
-                           }
+}
 function script:OnApplicationExit {
 	$script:ExitCode = 0 #Set the exit code for the Packager
-                           }
+}
+function Show-MessageBox {
+  <#
+    .SYNOPSIS 
+      Displays a MessageBox using Windows WinForms
+	  
+	.Description
+	  	This function helps display a custom Message box with the options to set
+	  	what Icons and buttons to use. By Default without using any of the optional
+	  	parameters you will get a generic message box with the OK button.
+	  
+	.Parameter Msg
+		Mandatory: This item is the message that will be displayed in the body
+		of the message box form.
+		Alias: M
+
+	.Parameter Title
+		Optional: This item is the message that will be displayed in the title
+		field. By default this field is blank unless other text is specified.
+		Alias: T
+
+	.Parameter OkCancel
+		Optional:This switch will display the Ok and Cancel buttons.
+		Alias: OC
+
+	.Parameter AbortRetryIgnore
+		Optional:This switch will display the Abort Retry and Ignore buttons.
+		Alias: ARI
+
+	.Parameter YesNoCancel
+		Optional: This switch will display the Yes No and Cancel buttons.
+		Alias: YNC
+
+	.Parameter YesNo
+		Optional: This switch will display the Yes and No buttons.
+		Alias: YN
+
+	.Parameter RetryCancel
+		Optional: This switch will display the Retry and Cancel buttons.
+		Alias: RC
+
+	.Parameter Critical
+		Optional: This switch will display Windows Critical Icon.
+		Alias: C
+
+	.Parameter Question
+		Optional: This switch will display Windows Question Icon.
+		Alias: Q
+
+	.Parameter Warning
+		Optional: This switch will display Windows Warning Icon.
+		Alias: W
+
+	.Parameter Informational
+		Optional: This switch will display Windows Informational Icon.
+		Alias: I
+
+	.Parameter TopMost
+		Optional: This switch will make the form stay on top until the user answers it.
+		Alias: TM	
+		
+	.Example
+		Show-MessageBox -Msg "This is the default message box"
+		
+		This example creates a generic message box with no title and just the 
+		OK button.
+	
+	.Example
+		$A = Show-MessageBox -Msg "This is the default message box" -YN -Q
+		
+		if ($A -eq "YES") {
+			..do something 
+		} 
+		else { 
+		 ..do something else 
+		} 
+
+		This example creates a msgbox with the Yes and No button and the
+		Question Icon. Once the message box is displayed it creates the A varible
+		with the message box selection choosen.Once the message box is done you 
+		can use an if statement to finish the script.
+		
+	.Notes
+		Created By Zachary Shupp
+		Email zach.shupp@hp.com		
+
+		Version: 1.0
+		Date: 9/23/2013
+		Purpose/Change:	Initial function development
+
+		Version 1.1
+		Date: 12/13/2013
+		Purpose/Change: Added Switches for the form Type and Icon to make it easier to use.
+
+		Version 1.2
+		Date: 3/4/2015
+		Purpose/Change: Added Switches to make the message box the top most form.
+				Corrected Examples
+		
+	.Link
+		http://msdn.microsoft.com/en-us/library/system.windows.forms.messagebox.aspx
+		
+  #>
+	Param (
+	[Parameter(Mandatory=$True)][Alias('M')][String]$Msg,
+	[Parameter(Mandatory=$False)][Alias('T')][String]$Title = "",
+	[Parameter(Mandatory=$False)][Alias('OC')][Switch]$OkCancel,
+	[Parameter(Mandatory=$False)][Alias('OCI')][Switch]$AbortRetryIgnore,
+	[Parameter(Mandatory=$False)][Alias('YNC')][Switch]$YesNoCancel,
+	[Parameter(Mandatory=$False)][Alias('YN')][Switch]$YesNo,
+	[Parameter(Mandatory=$False)][Alias('RC')][Switch]$RetryCancel,
+	[Parameter(Mandatory=$False)][Alias('C')][Switch]$Critical,
+	[Parameter(Mandatory=$False)][Alias('Q')][Switch]$Question,
+	[Parameter(Mandatory=$False)][Alias('W')][Switch]$Warning,
+	[Parameter(Mandatory=$False)][Alias('I')][Switch]$Informational,
+        [Parameter(Mandatory=$False)][Alias('TM')][Switch]$TopMost)
+	[Parameter(Mandatory=$False)][Alias('AS')][Switch]$AutoSize)
+
+	#Set Message Box Style
+	IF($OkCancel){$Type = 1}
+	Elseif($AbortRetryIgnore){$Type = 2}
+	Elseif($YesNoCancel){$Type = 3}
+	Elseif($YesNo){$Type = 4}
+	Elseif($RetryCancel){$Type = 5}
+	Else{$Type = 0}
+	
+	#Set Message box Icon
+	If($Critical){$Icon = 16}
+	ElseIf($Question){$Icon = 32}
+	Elseif($Warning){$Icon = 48}
+	Elseif($Informational){$Icon = 64}
+	Else { $Icon = 0 }
+	
+	#Loads the WinForm Assembly, Out-Null hides the message while loading.
+	[System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") | Out-Null
+	
+	If ($TopMost) {
+		#Creates a Form to use as a parent
+		$FrmMain = New-Object 'System.Windows.Forms.Form'
+		$FrmMain.TopMost = $true
+		
+		#Display the message with input
+		$Answer = [System.Windows.Forms.MessageBox]::Show($FrmMain, $MSG, $TITLE, $Type, $Icon)
+		
+		#Dispose of parent form
+		$FrmMain.Close()
+		$FrmMain.Dispose()
+	}
+	Else {
+		#Display the message with input
+		$Answer = [System.Windows.Forms.MessageBox]::Show($MSG , $TITLE, $Type, $Icon)			
+	}
+	
+	#Return Answer
+	Return $Answer
+}
 
 function Test-RegistryValue {
   <#
@@ -35,24 +190,22 @@ function Test-RegistryValue {
 }
 
 function Set-RegistryKey {
-    
   [CmdletBinding()]
-  param
-  (
+  param (
     [Object]$computername,
     [Object]$parentKey,
     [Object]$nameRegistryKey,
     [Object]$valueRegistryKey
   )
-try{    
+  try {    
         $remoteBaseKeyObject = [microsoft.win32.registrykey]::OpenRemoteBaseKey('LocalMachine',$computername)     
         $regKey = $remoteBaseKeyObject.OpenSubKey($parentKey,$true)
         $regKey.Setvalue("$nameRegistryKey", "$valueRegistryKey", [Microsoft.Win32.RegistryValueKind]::DWORD) 
         $remoteBaseKeyObject.close()
-    }
-    catch {
+  }
+  catch {
         $_.Exception
-    }
+  }
 }
 
 function Search-LastDrive
